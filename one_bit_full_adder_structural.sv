cf8ce511e66bbfecbf0107033fe10d6e7eeb94ac
Akona04 <129303298+Akona04@users.noreply.github.com>
2025-03-13 22:53:21 -0500

    Update one_bit_full_adder_structural.sv

--- one_bit_full_adder_structural.sv
+++ one_bit_full_adder_structural.sv
@@ -1,16 +1,13 @@
-module Main_Structural(S, Cout, A, B, Cin);
-    output S, Cout;
-    input A, B, Cin;
-
-    wire and_0_out, or_1_out, or_0_out, and_2_out, and_1_out, xor_0_out;
-
-    and (and_0_out, Cin, B);
-    and (and_2_out, Cin, A);
-    and (and_1_out, B, A);
-    or  (or_1_out, and_2_out, and_0_out, and_1_out);
-    assign Cout = or_1_out;
-    xor (xor_0_out, A, B);
-    or  (or_0_out, xor_0_out, Cin);
-    assign S = or_0_out;
-
+module \1_adder (S, Cout, A, B, Cin);
+  output S,  Cout;
+  input A, B, Cin;
+  wire and_0_out, or_0_out, xor_1_out, and_2_out, and_1_out, xor_0_out;
+  assign and_0_out = Cin & B;
+  assign or_0_out = and_2_out | and_0_out | and_1_out;
+  assign Cout = or_0_out;
+  assign xor_1_out = xor_0_out ^ Cin;
+  assign S = xor_1_out;
+  assign and_2_out = Cin & A;
+  assign and_1_out = B & A;
+  assign xor_0_out = A ^ B;
 endmodule
