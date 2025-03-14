074ed22e80a82464ec7ec5bff49b5cb1909be4a8
Akona04 <129303298+Akona04@users.noreply.github.com>
2025-03-13 22:54:11 -0500

    Update one_bit_full_adder_behavioral.sv

--- one_bit_full_adder_behavioral.sv
+++ one_bit_full_adder_behavioral.sv
@@ -1,8 +1,24 @@
-module Main_Behavioral(
-    output reg S, Cout,
-    input A, B, Cin
-);
-    always @(*) begin
-        {Cout, S} = A + B + Cin; // Full adder behavior using arithmetic operations
-    end
+module adder_using_always();
+  reg A, B, Cin; //innputs
+  reg Sum, Carry; //outputs
+
+  always @ (A or B or Cin) begin
+    {Carry, Sum} = A + B + Cin; 
+  end
+
+  initial begin
+    $monitor (" A = %b B = %b Cin = %b | Carry = %b Sum = %b", A, B, Cin, Carry, Sum);
+
+    #10 A = 0; B = 0; Cin = 0;
+    #10 A = 0; B = 0; Cin = 1;
+    #10 A = 0; B = 1; Cin = 0;
+    #10 A = 0; B = 1; Cin = 1;
+    #10 A = 1; B = 0; Cin = 0;
+    #10 A = 1; B = 0; Cin = 1;
+    #10 A = 1; B = 1; Cin = 0;
+    #10 A = 1; B = 1; Cin = 1;
+
+    #10 $finish; 
+  end
+
 endmodule
